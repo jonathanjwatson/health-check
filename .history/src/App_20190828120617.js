55ca82639c92adf37b866b11e350feec9b9ca2eb
Jonathan Watson <jonathan.watson@deluxe.com>
2019-08-28 12:14:45 -0400

    modified app.js

--- .history/src/App_20190828120617.js
+++ .history/src/App_20190828120617.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          this is a simple test
+          Learn React
         </a>
       </header>
     </div>
